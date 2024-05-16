238288c92acf78389813ab7b274c252edfe8a90c
John Lago <750845+Lagoja@users.noreply.github.com>
2024-05-16 16:12:10 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Source: https://github.com/Automattic/node-canvas?tab=readme-ov-file#quick-example
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(200, 200)
+const ctx = canvas.getContext('2d')
+
+// Write "Awesome!"
+ctx.font = '30px Impact'
+ctx.rotate(0.1)
+ctx.fillText('Awesome!', 50, 100)
+
+// Draw line under text
+var text = ctx.measureText('Awesome!')
+ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+ctx.beginPath()
+ctx.lineTo(50, 102)
+ctx.lineTo(50 + text.width, 102)
+ctx.stroke()
+
+// Draw cat with lime helmet
+loadImage('https://images-assets.nasa.gov/image/AFRC2024-0067-40/AFRC2024-0067-40~orig.jpg').then((image) => {
+  ctx.drawImage(image, 50, 0, 70, 70)
+
+  console.log('<img src="' + canvas.toDataURL() + '" />')
+})
